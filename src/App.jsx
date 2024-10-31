bfce088b07d0614eaed3d3b59c4d7eb06dd22972
iNJUSTiCE <injusticenubs@gmail.com>
2024-11-01 04:00:34 +0530

    stable mqqt cycle 1 /fixed success route:

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import HomePage from "./page/HomePage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import WeatherApp from "./page/WeatherApp";
+import SuccessPage from "./page/SuccessFull";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           />
 
           <Route path="/WeatherApp" element={<WeatherApp />} />
+          <Route path="/success" element={<SuccessPage />} />
         </Routes>
       </Router>
     </>
